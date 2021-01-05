cdd9d38ef72462d6624272ade15b1790a8b0d9b5
Night Fury ^^ <F1U7R2Y9@github.com>
2021-01-05 18:16:20 +0530

    Change it ,Thanks
    Make it look like a full Christmas Tree

---
+++ ChristmasTree.C
@@ -0,0 +1,50 @@
+//Created by NIGHT FURY 5.01.2021 F1U7R2Y9
+
+#include <stdio.h>
+#include <conio.h>
+
+    int main()
+
+    {
+        int r,c;
+        for(r=1;r<=10;r++)
+        {
+            for(c=1;c<=r;c++)
+        {
+            printf("*");
+        }
+            printf("\t\t\n");
+        }
+
+        for(r=1;r<=10;r++)
+        {
+            for(c=1;c<=r;c++)
+        {
+            printf("**");
+        }
+            printf("\t\t\n");
+        }
+
+         for(r=1;r<=10;r++)
+        {
+            for(c=1;c<=r;c++)
+        {
+            printf("***");
+        }
+            printf("\t\t\n");
+        }
+
+        for(r=1;r<=10;r++)
+        {
+            for(c=1;c<=r;c++)
+        {
+            printf("****");
+        }
+            printf("\t\t\n");
+        }
+
+
+
+        
+        return 0;
+    }
---
+++ ChristmasTwee.C
@@ -0,0 +1,14 @@
+//Created by NIGHT FURY 5.01.2021 F1U7R2Y9
+
+#include <stdio.h>
+#include <conio.h>
+
+    int main()
+
+    {
+        
+
+
+        
+        return 0;
+    }
