9f0af1272696e11738dfe156f390f21f842aec4d
Night Fury ^^ <71229752+F1U7R2Y9@users.noreply.github.com>
2021-01-23 12:45:33 +0530

    Update ChristmasTree.C

--- ChristmasTree.C
+++ ChristmasTree.C
@@ -1,49 +1,89 @@
-//Thanks to YASH NARODIA for changing the code :P
-//Repository BY NIGHTFURY
-
 #include <stdio.h>
-int main() {
+
+int main() 
+{
    int i, space, rows, k = 0;
-   for (i = 1; i <= 5; ++i, k = 0) {
-      for (space = 1; space <= 10- i; ++space) {
+
+   printf("\n\tMarry Christmas ! yay !");
+   printf("\n\n");
+
+   for (i = 1; i <= 5; ++i, k = 0) 
+
+   {
+
+      for (space = 1; space <= 10- i; ++space) 
+
+      {
          printf("  ");
       }
-      while (k != 2 * i - 1) {
+
+      while (k != 2 * i - 1)
+
+       {
+         printf("* ");
+         ++k;
+       }
+
+      printf("\n");
+
+   }
+
+   for (i = 1; i <= 7; ++i, k = 0)
+
+    {
+
+      for (space = 1; space <= 10- i; ++space) 
+
+      {
+         printf("  ");
+      }
+
+      while (k != 2 * i - 1) 
+
+      {
          printf("* ");
          ++k;
       }
       printf("\n");
    }
-   for (i = 1; i <= 7; ++i, k = 0) {
-      for (space = 1; space <= 10- i; ++space) {
+
+   for (i = 1; i <= 10; ++i, k = 0) 
+
+   {
+      for (space = 1; space <= 10 - i; ++space) 
+
+      {
          printf("  ");
       }
-      while (k != 2 * i - 1) {
+
+      while (k != 2 * i - 1) 
+
+      {
          printf("* ");
          ++k;
       }
       printf("\n");
    }
-   for (i = 1; i <= 10; ++i, k = 0) {
-      for (space = 1; space <= 10 - i; ++space) {
-         printf("  ");
-      }
-      while (k != 2 * i - 1) {
-         printf("* ");
-         ++k;
-      }
-      printf("\n");
-   }
+
     for(i=1; i<=5; i++)
-    {    for (space = 1; space <= 8; ++space) 
+
+    {    
+        for (space = 1; space <= 8; ++space) 
+
         {
             printf("  ");
         }
+
         for(int j=1; j<=5; j++)
+
         {
             printf("*");
         }
+
         printf("\n");
+
+
     }
+    
    return 0;
 }
